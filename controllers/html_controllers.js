c8afbcef92ea874d1a2ae73b4ff7e53d80c28251
ukgt <ukgt2019@gmail.com>
2019-03-08 12:29:57 -0500

    fuel service handlebars

--- controllers/html_controllers.js
+++ controllers/html_controllers.js
@@ -1,32 +1,58 @@
 var express = require("express");
 const router = express.Router();
+const fuel = require("../models");
 
-router.get("/", function(req,res){
-     res.render("index", {
-          title: "Home",
-          
-     })
-});
+router.post("/fuel", function (req, res) {
+     //    module.exports = function (app) {
 
-router.get("/car", function (req, res) {
-     res.render("car", {
-          title: "Car Details"
-     })
+     fuel.Fuel.create(req.body).then(function (Fuel) {
+          res.json(Fuel);
+     });
+
+     //    }
 });
 
 router.get("/fuel", function (req, res) {
      res.render("fuel", {
-          title: "Fuel Details"
+          title: "Fuel Details",
+          fuel: [{
+               item: ["Add New Record"],
+
+          }]
      })
 });
 
 router.get("/service", function (req, res) {
      res.render("service", {
-          title: "Service Details"
+          title: "Service Details",
+          service: [{
+               item: ["Add New Record"],
+               items: ["Service Entries"]
+          }]
      })
 });
 
-router.post("/")
+router.post("/service", function (req, res) {
+     res.render("service", {
+          title: "Service Details",
+          service: [{
+               item: ["Add New Record"],
+               items: ["Service Entries"]
+          }]
+     })
+     req.body.user_input("Add New Record")
+});
+router.get("/service", function (req, res) {
+     res.render("service", {
+          title: "Service Details",
+          service: [{
+
+               items: ["Service Entry"]
+          }]
+     })
+});
+
+
 
 
 
