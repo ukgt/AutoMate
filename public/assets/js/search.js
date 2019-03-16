c8aa71548c463f9e1266310af8c0f9540d935f84
Kyle Kinzey <Kyle@Kyles-MacBook-Pro-2.local>
2019-03-15 22:07:53 -0400

    pushing to merg

---
+++ public/assets/js/search.js
@@ -0,0 +1,24 @@
+//create a function that reaches out to the sql data base and return records in descending order
+
+//function to find all records for car
+db.Automate.findAll().then(services => {
+    // projects will be an array of all instances
+    return serviceItems
+  })
+//list record and description
+ 
+
+//service
+
+
+
+//gas
+
+db.Automate.findAll({ where: { name: 'fuel' } }).then(fuels => {
+    // projects will be an array of Project instances with the specified name
+    return fuelItems
+
+  })
+
+
+
